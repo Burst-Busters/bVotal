c0da62b399ccf4d1fa2d692f292bbdb6191cd800
RcDev <romuloctba@gmail.com>
2020-10-10 14:04:46 -0300

    add first page with material UI

--- packages/react-voting-app/src/pages/start/StartPage.tsx
+++ packages/react-voting-app/src/pages/start/StartPage.tsx
@@ -1,33 +1,92 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './StartPage.css';
 import {FormattedMessage } from 'react-intl';
 import {hashId} from '@bvotal/common'
+import { Card, CardContent, CardMedia, Divider, Fab, List, ListItem, ListItemText, makeStyles, Paper, Typography } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  startPage: {
+    width: 'auto',
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 800,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+    position: `relative`,
+  },
+  card: {
+    display: 'flex',
+    textAlign: `left`,
+    marginTop: theme.spacing(2),
+  },
+  cardDetails: {
+    flex: 1,
+  },
+  cardMedia: {
+    width: 160,
+  },
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(-1),
+    right: theme.spacing(2),
+  },
+}))
 function StartPage() {
-
+  const classes = useStyles();
   const hash = hashId({id:'1244', dob:'12-12-2345'})
 
   return (
-    <div className="StartPage">
-      <header className="StartPage-header">
-        <img src={logo} className="StartPage-logo" alt="logo" />
-        <p>
-          <FormattedMessage id="StartPage.initialText"
-            defaultMessage="Edit <code>src/StartPage.js</code> and save to reload {StartPageName}."
-            values={{
-              code: (chunks: string[]) => <code>{chunks}</code>, // this is needed to declare the <code> tag for the formattedMessage component!!
-              StartPageName: `bVotal`
-            }}
-            description="Initial text on the StartPage.tsx" />
-        </p>
-        <h2>
-        <FormattedMessage id="StartPage.exampleHash"
-          defaultMessage="Example Hash:"
-          description="example hash on homepage" />
-        </h2>
-        <p>{hash}</p>
-      </header>
+    <div className={classes.startPage}>
+      <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5" align="left">
+            Vote in 3 easy steps
+          </Typography>
+            <Card className={classes.card}>
+              <div className={classes.cardDetails}>
+                <CardContent>
+                  <List className={classes.root}>
+                    <ListItem alignItems="flex-start">
+                      <ListItemText 
+                        primary="1. Create your Passphrase" 
+                        secondary="Securely create your account by getting na exclusive passphrase" />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                    <ListItem alignItems="flex-start">
+                      <ListItemText 
+                          primary="2. Create your PIN" 
+                          secondary="The PIN allows you to place your vote" />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                    <ListItem alignItems="flex-start">
+                      <ListItemText 
+                        primary="3. Add your documents" 
+                        secondary="Proof your identity by providing your doc ID" />
+                    </ListItem>
+                  </List>
+                </CardContent>
+              </div>
+              <CardMedia className={classes.cardMedia} image={`http://placehold.it/160x300`} title="asdasdas" />
+            </Card>
+            <Fab className={classes.fab} size="large" color="secondary" aria-label="go">
+              GO!
+            </Fab>
+      </Paper>
     </div>
   );
 }
