c7f77d229e8ce4ee65e84cff78fbb3f00a903712
ohager <oliver.hager@dextra-sw.com>
2020-10-10 16:36:52 -0300

    feat: moved bootstrapper to ea-service

---
+++ packages/eligibility-service/src/commands/bootstrap/bootstrapper.js
@@ -0,0 +1,79 @@
+const {BurstService} = require("@burstjs/core");
+const {BurstValue} = require("@burstjs/util");
+const {
+    PassPhraseGenerator,
+    generateMasterKeys,
+    getAccountIdFromPublicKey
+} = require("@burstjs/crypto");
+
+const {api, NODE} = require('./config');
+
+const sleep = time => new Promise((resolve) => {
+    setTimeout(resolve, time || 1000);
+});
+
+class Bootstrapper {
+    constructor({campaignName, targetBalance}) {
+        this._campaignName = campaignName;
+        this._targetBalance = BurstValue.fromBurst(targetBalance)
+    }
+
+    #createRandomAccount() {
+        const seed = crypto.randomBytes(64)
+        this._passphrase = new PassPhraseGenerator().generatePassPhrase(seed).join(" ");
+        const keys = generateMasterKeys(this._passphrase);
+        this._publicKey = keys.publicKey;
+        this._accountID = getAccountIdFromPublicKey(keys.publicKey);
+    }
+
+    async #premine() {
+        let currentBalance = BurstValue.fromBurst(0)
+        while (currentBalance.lessOrEqual(this._targetBalance)) {
+            await this.forgeBlock()
+            currentBalance = await this.getBalanceBurst()
+        }
+    }
+
+    static async run({campaignName, targetBalance}) {
+        const instance = new Bootstrapper({campaignName, targetBalance})
+        instance.#createRandomAccount()
+        await instance.#premine({targetBalance})
+        return instance;
+    }
+
+    forgeBlock = async () => {
+        let burstService = new BurstService({nodeHost: NODE});
+        await burstService.send('submitNonce', {
+            secretPhrase: this.passphrase,
+            nonce: 0
+        })
+
+        await sleep(500)
+    }
+
+    getBalanceBurst = async () => {
+        const {balanceNQT} = await api.account.getAccountBalance(this.accountID);
+        return BurstValue.fromPlanck(balanceNQT);
+    }
+
+    get passphrase() {
+        return this._passphrase
+    }
+
+    get campaignName() {
+        return this._campaignName;
+    }
+
+    get accountId() {
+        return getAccountIdFromPublicKey(this.publicKey)
+    }
+
+    get publicKey() {
+        const keys = generateMasterKeys(this._passphrase);
+        return keys.publicKey;
+    }
+}
+
+module.exports = {
+    Bootstrapper
+}
