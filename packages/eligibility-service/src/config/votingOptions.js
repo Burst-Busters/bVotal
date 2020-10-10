4d2dbc80736e59217dfa25c7935c06e4dd6b82a8
ohager <oliver.hager@dextra-sw.com>
2020-10-10 14:47:03 -0300

    feat: added send activation message

---
+++ packages/eligibility-service/src/config/votingOptions.js
@@ -0,0 +1,10 @@
+const VotingOptions = [
+    {key: 1, title: "Option 1", desc: "This is option 1 - best candidate ever"},
+    {key: 2, title: "Option 2", desc: "This one saves the nation"},
+    {key: 3, title: "Option 3", desc: "This egomaniac politician wants your money"},
+    {key: 4, title: "Option 4", desc: "This guy is against Option 1"},
+]
+
+module.exports = {
+    VotingOptions
+}
