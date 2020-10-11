f78776a88d43497a16719e9f5e1a651f7c73c0a7
ohager <oliver.hager@dextra-sw.com>
2020-10-11 13:54:58 -0300

    storing password securely, simplified pin creation

--- packages/react-voting-app/src/pages/create-pin/CreatePinPage.tsx
+++ packages/react-voting-app/src/pages/create-pin/CreatePinPage.tsx
@@ -1,219 +1,219 @@
-import React, { useEffect, useState } from 'react';
-import { Backdrop, Button, Card, CardContent, CircularProgress, Fab, FormControl, InputAdornment, InputLabel, makeStyles, OutlinedInput, Paper, Typography } from '@material-ui/core';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import {
+    Backdrop,
+    Button,
+    Card,
+    CardContent,
+    CircularProgress,
+    Fab,
+    FormControl,
+    InputAdornment,
+    InputLabel,
+    makeStyles,
+    OutlinedInput,
+    Paper,
+    Typography
+} from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
-import { useHistory } from 'react-router-dom';
-import api, { VotingOption } from '../../services/api';
+import {useHistory} from 'react-router-dom';
+import api, {VotingOption} from '../../services/api';
 
 const useStyles = makeStyles((theme) => ({
-  CreatePinPage: {
-    width: 'auto',
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-      width: 800,
-      marginLeft: 'auto',
-      marginRight: 'auto',
+    CreatePinPage: {
+        width: 'auto',
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+            width: 800,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
     },
-  },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
+    paper: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+            marginTop: theme.spacing(6),
+            marginBottom: theme.spacing(6),
+            padding: theme.spacing(3),
+        },
+        position: `relative`,
     },
-    position: `relative`,
-  },
-  card: {
-    display: 'flex',
-    textAlign: `left`,
-    marginTop: theme.spacing(2),
-  },
-  pinInput: {
-    marginTop: theme.spacing(6),
-  },
-  pinConfirmInput: {
-      marginTop: theme.spacing(6),
-  },
-  pinConfirmLabel: {
-    marginTop: theme.spacing(6),
-},
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-  buttonInput: {
-    display: `block`,
-    clear: `both`, 
-    marginTop: theme.spacing(3)
-  },
-  cardDetails: {
-    flex: 1,
-    textAlign: 'center',
-  },
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-  fab: {
-    position: 'absolute',
-    bottom: theme.spacing(-1),
-    right: theme.spacing(2),
-  },
-  passphraseStats: {
-    marginTop: theme.spacing(3),
-  },
-  chip: {
-    marginRight: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-  }
+    card: {
+        display: 'flex',
+        textAlign: `left`,
+        marginTop: theme.spacing(2),
+    },
+    pinInput: {
+        marginTop: theme.spacing(6),
+    },
+    pinConfirmInput: {
+        marginTop: theme.spacing(6),
+    },
+    pinConfirmLabel: {
+        marginTop: theme.spacing(6),
+    },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+    buttonInput: {
+        display: `block`,
+        clear: `both`,
+        marginTop: theme.spacing(3)
+    },
+    cardDetails: {
+        flex: 1,
+        textAlign: 'center',
+    },
+    root: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+    fab: {
+        position: 'absolute',
+        bottom: theme.spacing(-1),
+        right: theme.spacing(2),
+    },
+    passphraseStats: {
+        marginTop: theme.spacing(3),
+    },
+    chip: {
+        marginRight: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+    }
 }))
-export type CreatePinPageProps = { 
-  location: { 
-    state: {
-      passphrase: string[];
-      hashId: string;
-    } 
-  }
+export type CreatePinPageProps = {
+    location: {
+        state: {
+            passphrase: string;
+            hashId: string;
+        }
+    }
 }
+
 function CreatePinPage(props: CreatePinPageProps) {
-  const classes = useStyles();
-  const { location } = props;
-  const history = useHistory();
-  const passphrase = location.state.passphrase;
-  const hashId = location.state.hashId;
-  const [loading, setLoading] = useState(false);
-  const [pin, setPin] = useState<string>();
-  const [publicKey, setPublicKey] = useState<string>();
-  const [validPin, setValidPin] = useState(false);
-  const [votingOptions, setVotingOptions] = useState<VotingOption[]>();
-  const [votingAddress, setVotingAddress] = useState<string>();
-  const [isPinCreated, setIsPinCreated] = useState(false);
+    const classes = useStyles();
+    const {location} = props;
+    const history = useHistory();
+    const passphrase = location.state.passphrase;
+    const hashId = location.state.hashId;
+    const [loading, setLoading] = useState(false);
+    const [pin, setPin] = useState<string>();
+    const [publicKey, setPublicKey] = useState<string>();
+    const [validPin, setValidPin] = useState(false);
+    const [votingOptions, setVotingOptions] = useState<VotingOption[]>();
+    const [votingAddress, setVotingAddress] = useState<string>();
 
-  const doRegister = async (hashId: string, publicKey: string) => {
-    const activationMessage = await api.register(hashId, publicKey);
-    console.log('registerResponse ', activationMessage);
-    setVotingOptions(activationMessage.vopts);
-    setVotingAddress(activationMessage.vaddrs);
-  }
+    const doRegister = async (hashId: string, publicKey: string) => {
+        try {
+            setLoading(true)
+            const {vaddrs, vopts} = await api.register(hashId, publicKey);
+            setVotingOptions(vopts);
+            setVotingAddress(vaddrs);
+        } catch (e) {
+            console.error(e)
+            // TODO: how to handle?
+        } finally {
+            setLoading(false)
+        }
+    }
 
-  useEffect(() => {
-    console.log(`the passphrase is `, passphrase);
-    const certificates = api.getMasterKeys(passphrase);
-    setPublicKey(certificates.publicKey);
-    console.log('Got certificates! ', certificates);
-  }, [])
+    useEffect(() => {
+        const {publicKey} = api.getMasterKeys(passphrase);
+        setPublicKey(publicKey);
+    }, [])
 
-  useEffect(() => {
-    if (!hashId || !publicKey) return;
-  
-    doRegister(hashId, publicKey);
-  }, [publicKey]);
+    useEffect(() => {
+        if (!hashId || !publicKey) return;
+        doRegister(hashId, publicKey);
+    }, [publicKey]);
 
-  const handlePinChange = (value: string) => {
-    console.log(`PIN is ${value}`);
-    setPin(value);
-  }
-  const handleConfirmPinChange = (value: string) => {
-      console.log(`Confirm PIN is ${value}`);
-      if (pin === value) {
-          setValidPin(true)
-      }
-  }
-  const handleCreateButton = () => {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        alert(`OK`);
-        setIsPinCreated(true);
-      }, 1000);
-  }
+    const handlePinChange = (pin: string) => {
+        setPin(pin);
+    }
+    const handleConfirmPinChange = (confirmingPin: string) => {
+        setValidPin(pin === confirmingPin)
+    }
 
-  const handleFabClick = () => {
-    if (!passphrase || !pin) return;
+    const handleFabClick = () => {
 
-    const encripted = api.encriptPassphrase(passphrase, pin);
-    window.localStorage.setItem('encriptPassphrase', encripted);
-    history.push({
-      pathname: `/vote`,
-      state: {
-        votingOptions,
-        votingAddress,
-      }
-    });
-  }
+        api.storePassphrase(passphrase, pin!)
 
+        history.push({
+            pathname: `/vote`,
+            state: {
+                votingOptions,
+                votingAddress,
+            }
+        });
+    }
 
-  return (
-    <div className={classes.CreatePinPage}>
-      <Paper className={classes.paper}>
-          <Typography component="h1" variant="h5" align="center">
-          <Fab size="medium" disabled color="primary" aria-label="1">
-              2
-            </Fab> Create your PIN
-          </Typography>
-            <Card className={classes.card}>
-              <div className={classes.cardDetails}>
-                <CardContent>
-                   <Typography component="p" variant="body2" align="center">
-                        Create a 5 digit PIN code that you will use to Vote
-                    </Typography>
-                    <FormControl className={classes.pinInput} variant="outlined">
-                    <InputLabel htmlFor="outline-pin">PIN</InputLabel>
-                    <OutlinedInput
-                        id="outline-pin"
-                        type={'text'}
-                        onChange={e => handlePinChange(e.target.value)}
-                        labelWidth={30}
-                        endAdornment={
-                        <InputAdornment position="end">
-                            <EditIcon />
-                        </InputAdornment>
-                        }
-                    />
-                    </FormControl>
-                    <Typography className={classes.pinConfirmLabel} component="p" variant="body2" align="center">
-                        Confirm your PIN by typing it again bellow:
-                    </Typography>
-                    <FormControl className={classes.pinConfirmInput} variant="outlined">
-                    <InputLabel htmlFor="outline-confirm-pin">Confirm PIN</InputLabel>
-                    <OutlinedInput
-                        id="outline-confirm-pin"
-                        type={'text'}
-                        onChange={e => handleConfirmPinChange(e.target.value)}
-                        labelWidth={90}
-                        endAdornment={
-                        <InputAdornment position="end">
-                            <EditIcon />
-                        </InputAdornment>
-                        }
-                    />
-                    </FormControl>
-                    <FormControl className={classes.buttonInput}>
-                        <Button disabled={!validPin} onClick={handleCreateButton} variant="outlined" color="primary">
-                            Save PIN
-                        </Button>
-                    </FormControl>
-                    <Backdrop className={classes.backdrop} open={loading}>
-                        <CircularProgress color="inherit" />
-                    </Backdrop>
-                </CardContent>
-              </div>
-            </Card>
-            <Fab 
-                disabled={!isPinCreated}
-                onClick={handleFabClick}
-                className={classes.fab}
-                size="large"
-                color="secondary"
-                aria-label="go">
-                Vote!
-            </Fab>
-      </Paper>
-    </div>
-  );
+    return (
+        <div className={classes.CreatePinPage}>
+            <Paper className={classes.paper}>
+                <Typography component="h1" variant="h5" align="center">
+                    <Fab size="medium" disabled color="primary" aria-label="1">
+                        2
+                    </Fab> Create your PIN
+                </Typography>
+                <Card className={classes.card}>
+                    <div className={classes.cardDetails}>
+                        <CardContent>
+                            <Typography component="p" variant="body2" align="center">
+                                Create a 5 digit PIN code that you will use to Vote
+                            </Typography>
+                            <FormControl className={classes.pinInput} variant="outlined">
+                                <InputLabel htmlFor="outline-pin">PIN</InputLabel>
+                                <OutlinedInput
+                                    id="outline-pin"
+                                    type={'text'}
+                                    onChange={e => handlePinChange(e.target.value)}
+                                    labelWidth={30}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <EditIcon/>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <Typography className={classes.pinConfirmLabel} component="p" variant="body2"
+                                        align="center">
+                                Confirm your PIN by typing it again bellow:
+                            </Typography>
+                            <FormControl className={classes.pinConfirmInput} variant="outlined">
+                                <InputLabel htmlFor="outline-confirm-pin">Confirm PIN</InputLabel>
+                                <OutlinedInput
+                                    id="outline-confirm-pin"
+                                    type={'text'}
+                                    onChange={e => handleConfirmPinChange(e.target.value)}
+                                    labelWidth={90}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <EditIcon/>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <Backdrop className={classes.backdrop} open={loading}>
+                                <CircularProgress color="inherit"/>
+                            </Backdrop>
+                        </CardContent>
+                    </div>
+                </Card>
+                <Fab
+                    disabled={!validPin}
+                    onClick={handleFabClick}
+                    className={classes.fab}
+                    size="large"
+                    color="secondary"
+                    aria-label="go">
+                    Register
+                </Fab>
+            </Paper>
+        </div>
+    );
 }
 
 export default CreatePinPage;
